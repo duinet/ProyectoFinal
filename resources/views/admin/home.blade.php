a8b786e0fa104396544f04550a67fb9dde26bd1c
duinet <jaimelozano7890@gmail.com>
2021-04-26 21:38:28 +0200

    WebEstatica user1

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -13,7 +13,7 @@
                                     </div>
                                     <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xl-6 benvingut">
                                         {{-- <h1 class="text-center">Benvingut {{ $user->name }}!</h1> --}}
-                                        <h1 class="text-center">Benvingut User!</h1>
+                                        <h1 class="text-center">Benvingut User1!</h1>
                                     </div>
                                 </div>
                             </div>
