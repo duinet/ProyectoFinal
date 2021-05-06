cc43c0a99a762c3e5e0002dd0e27dbb6126c1239
duinet <jaimelozano7890@gmail.com>
2021-05-06 16:51:07 +0200

    Version proyecto 2.0

--- resources/views/admin/Layout/dashboard.blade.php
+++ resources/views/admin/Layout/dashboard.blade.php
@@ -39,12 +39,6 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav ml-auto">
-                        <li class="nav-link d-none d-md-none d-lg-block d-xl-block d-xxl-block">
-                            <a><i class="fas fa-file-pdf"></i></a>
-                        </li>
-                        <li class="nav-link d-none d-md-none d-lg-block d-xl-block d-xxl-block">
-                            <a><i class="fas fa-file-excel"></i></a>
-                        </li>
                     <li class="nav-item">
                         <a class="nav-link d-none d-md-none d-lg-block d-xl-block d-xxl-block" data-widget="fullscreen" href="#" role="button">
                             <i class="fas fa-expand-arrows-alt"></i>
@@ -160,7 +154,5 @@
             <script src="{{ asset('assets/dashboard/dist/js/localDate.js') }}"></script>
         @endif
 
-        {{-- SweetAlert2 --}}
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     </body>
 </html>
